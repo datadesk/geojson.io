04e8de16033fa79998c7d17e96ccf25e41e23e62
Ken Schwencke <schwank@gmail.com>
2014-02-10 22:34:56 -0800

    OK, since we use CloudFront, I have to use the s3 bucket directly or change our cloudfront config to forward querystrings. Just going with S3 bucket for now

--- src/ui/layer_switch.js
+++ src/ui/layer_switch.js
@@ -4,9 +4,8 @@ module.exports = function(context) {
 
         var layers = [{
             title: 'Quiet LA',
-            layer: L.tileLayer('http://{s}.latimes.com/quiet-la-0.4.0/{z}/{x}/{y}.png', {
-                attribution: 'Map data: (c) <a href="http://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.</div>',
-                subdomains: ['tiles1','tiles2','tiles3','tiles4']
+            layer: L.tileLayer('https://s3-us-west-2.amazonaws.com/tiles.latimes.com/quiet-la-0.4.0/{z}/{x}/{y}.png', {
+                attribution: 'Map data: (c) <a href="http://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.</div>'
             })
         }];
 
